2ccc66d0f59f1359827550db04ef549d0b5f08bd
Tobias Wiklund <tobias.wiklund@gmail.com>
2017-11-24 12:58:15 +0100

    more indentation

---
+++ sites/default/files/php/twig/5a12cc64e77c4_paragraph--push.html.twig_Crhomyk1-utYGD5YGwvI6qCcE/rnlJff_0RFNX-TC8IE26bh9wBi0_Xj_8RNGkI9faFCA.php
@@ -0,0 +1,97 @@
+<?php
+
+/* themes/iths/templates/paragraphs/paragraph--push.html.twig */
+class __TwigTemplate_2dd52e73fc8aa81e442888003035b935c6d1b55674ee41d64141ee0fce991bf1 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<div class=\"column push\">
+ <div class=\"outer-overlay\">   
+  ";
+        // line 3
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content"] ?? null), "field_image", array()), "html", null, true));
+        echo " 
+  <div class=\"overlay\"></div>  
+    <div class=\"push-text\">
+      ";
+        // line 6
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content"] ?? null), "field_link", array()), "html", null, true));
+        echo " 
+    </div> 
+ </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/iths/templates/paragraphs/paragraph--push.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  53 => 6,  47 => 3,  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<div class=\"column push\">
+ <div class=\"outer-overlay\">   
+  {{content.field_image}} 
+  <div class=\"overlay\"></div>  
+    <div class=\"push-text\">
+      {{content.field_link}} 
+    </div> 
+ </div>
+</div>
+", "themes/iths/templates/paragraphs/paragraph--push.html.twig", "/Users/tobbe/www/labb2_drupal/themes/iths/templates/paragraphs/paragraph--push.html.twig");
+    }
+}
---
+++ sites/default/files/php/twig/5a12cc64e77c4_paragraph--pushes.html.tw_K_tQgwpj5_cGn3BpN3Usz-BwQ/t9yi_fpunnkU-BcelWNp0l8nrUqAIaxz-o3vFWSbT8A.php
@@ -0,0 +1,88 @@
+<?php
+
+/* themes/iths/templates/paragraphs/paragraph--pushes.html.twig */
+class __TwigTemplate_c9d1c77b720dbd617511b42b40c9d64c8fc98c019c3f4589253b1e8521694df4 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<div class=\"columns\">
+\t<div class=\"column is-10 is-offset-1\">
+    <div class=\"columns pushes\">
+\t   ";
+        // line 4
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["content"] ?? null), "html", null, true));
+        echo "    
+    </div>
+\t</div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/iths/templates/paragraphs/paragraph--pushes.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  48 => 4,  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<div class=\"columns\">
+\t<div class=\"column is-10 is-offset-1\">
+    <div class=\"columns pushes\">
+\t   {{content}}    
+    </div>
+\t</div>
+</div>", "themes/iths/templates/paragraphs/paragraph--pushes.html.twig", "/Users/tobbe/www/labb2_drupal/themes/iths/templates/paragraphs/paragraph--pushes.html.twig");
+    }
+}
